365ca9563c93e344603cf091cac227155825b40c
Miha Urbanija <10325909+mihaur@users.noreply.github.com>
2019-12-04 19:38:05 +0100

    Add 4 javascript  frameworks and updat tests * apollo-server-express * apollo-server-fastify * express-graphql * fastify-gql * remove empty scalar types from expected schema (ID and Float)

---
+++ frameworks/fastify-gql/app.js
@@ -0,0 +1,93 @@
+'use strict'
+
+const app = require('fastify')()
+const fastifyGQL = require('fastify-gql')
+
+const schema = `
+  type Query {
+    artist(name: String!): Artist
+    artists: [Artist]
+  }
+
+  type Mutation {
+    like(artist: String!, song: String!): Song
+  }
+
+  type Artist {
+    name: String!
+    songs: [Song]
+    origin: [String]
+  }
+
+  type Song {
+    name: String!
+    artist: Artist
+    duration: Int
+    release: Date
+    likes: Int
+  }
+
+  scalar Date
+`
+
+const artists = [
+  { name: 'Fazerdaze', origin: ['Morningside', 'Auckland', 'New Zealand'] },
+  { name: 'Viagra Boys', origin: ['Stockholm', 'Sweden'] }
+]
+
+const songs = [
+  { name: 'Jennifer', artist: 'Fazerdaze', duration: 240, released: '2017-05-05T00:00:00.000Z', likes: 0 },
+  { name: 'Lucky Girl', artist: 'Fazerdaze', duration: 170, released: '2017-05-05T00:00:00.000Z', likes: 0 },
+  { name: 'Friends', artist: 'Fazerdaze', duration: 194, released: '2017-05-05T00:00:00.000Z', likes: 0 },
+  { name: 'Reel', artist: 'Fazerdaze', duration: 193, released: '2015-11-02T00:00:00.000Z', likes: 0 },
+  { name: 'Down In The Basement', artist: 'Viagra Boys', duration: 216, released: '2018-09-28T00:00:00.000Z', likes: 0 },
+  { name: 'Frogstrap', artist: 'Viagra Boys', duration: 195, released: '2018-08-28T00:00:00.000Z', likes: 0 },
+  { name: 'Worms', artist: 'Viagra Boys', duration: 208, released: '2018-09-28T00:00:00.000Z', likes: 0 },
+  { name: 'Amphetanarchy', artist: 'Viagra Boys', duration: 346, released: '2018-09-28T00:00:00.000Z', likes: 0 }
+]
+
+const resolvers = {
+  Query: {
+    artist: (_, { name }) => artists.find(a => name === a.name),
+    artists: () => artists
+  },
+  Mutation: {
+    like: (_, { artist, song }) => {
+      const likedSong = songs.find(s => (artist === s.artist) && (song === s.name))
+      if (likedSong) {
+        ++likedSong.likes
+      }
+      return likedSong
+    }
+  },
+  Artist: {
+    songs: (artist) => songs.filter(s => s.artist === artist.name)
+  },
+  Song: {
+    artist: (song) => songs.filter(s => s.artist === song.name)
+  }
+}
+
+// fastify and fastify-gql do not support application/graphql parser
+if (!app.hasContentTypeParser('application/graphql')) {
+  app.addContentTypeParser('application/graphql', function (req, done) {
+    var body = ''
+    req.on('data', function (data) {
+      body += data
+    })
+    req.on('end', function () {
+      done(null, { query: body.replace('=', ' ') })
+    })
+    req.on('error', done)
+  })
+}
+
+app
+  .get('/', (request, reply) => reply.send())
+  .register(fastifyGQL, { schema, resolvers, graphiql: false })
+  .listen(3000, '0.0.0.0')
+  .then((address) => console.log(`GraphQL API server is listening at ${address}/graphql`))
+  .catch(err => {
+    console.error('Error starting server:', err)
+    process.exit(1)
+  })
